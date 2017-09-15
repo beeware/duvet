95a90c9681ee1bd8a2196d28d31d6040dbde2e0a
stevemolloy <sdmolloy@gmail.com>
2017-09-15 22:33:43 +0200

    Added some simple coverage files and data.

---
+++ tests/test_code_under_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from code_under_test import fully_covered_add, non_fully_covered_add
+
+class TestSuite(unittest.TestCase):
+    def test_fully_covered_add(self):
+        result = fully_covered_add(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_non_fully_covered_add(self):
+        result = non_fully_covered_add(2, 3)
+        self.assertEqual(result, 5)
